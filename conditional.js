f2fd677871a65954557c0987ed25a158dab55420
onik-nandi <iamoniknandi@gamil.com>
2025-07-29 20:43:15 +0600

    task 4 doen

--- conditional.js
+++ conditional.js
@@ -1,3 +1,4 @@
+// Task no-1
 var burgerPrice = 600;
 var cokePrice = 30;
 if (burgerPrice > 500) {
@@ -6,7 +7,7 @@ if (burgerPrice > 500) {
   console.log(cokePrice);
 }
 
-Task - 3;
+// Task - 3;
 
 var marks = 40;
 
@@ -21,3 +22,26 @@ if (marks >= 90 && marks <= 100) {
 } else {
   console.log("Fail");
 }
+// Tasks  4 nested
+
+var myMarks = 81;
+var freindMarks = 81;
+
+if (myMarks > 80) {
+  if (freindMarks > 80) {
+    console.log("Go for lunch");
+  } else {
+    if (freindMarks > 60) {
+      console.log("Good Luck next Time");
+    } else {
+      if (freindMarks >= 40) {
+        console.log("SMS Keep Unseen");
+      } else {
+        console.log("block kha shala");
+      }
+    }
+  }
+} else {
+  console.log("Go home & Sleep");
+}
+
